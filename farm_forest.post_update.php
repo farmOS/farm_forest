dca1c887d10129d8b24510b9c7d99a5dfa49fba6
Michael Stenta <mike@mstenta.net>
2024-02-04 08:09:05 -0500

    Fix Error: Class "Drupal\migrate_plus\Entity\Migration" not found
    This happens if the migrate_plus module is not installed when
    the update hook runs.

--- farm_forest.post_update.php
+++ farm_forest.post_update.php
@@ -5,13 +5,11 @@
  * Post update hooks for the farm_forest module.
  */
 
-use Drupal\migrate_plus\Entity\Migration;
-
 /**
  * Uninstall v1 migration config.
  */
 function farm_forest_post_update_uninstall_v1_migration(&$sandbox) {
-  $config = Migration::load('farm_migrate_area_forest');
+  $config = \Drupal::configFactory()->getEditable('migrate_plus.migration.farm_migrate_area_forest');
   if (!empty($config)) {
     $config->delete();
   }
